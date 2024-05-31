74d4c0301facd54a4b280a6819468453529a469f
Doomer0k <v_shnitkov@mail.ru>
2024-05-31 04:51:56 +0300

    Добавлен вывод общей информации о задании

---
+++ GIT/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #10. GIT\n";
+    cout << "Variant #3. precipitation\n";
+    cout << "Author: Shnitkov Vadim\n";
+    return 0;
+}
